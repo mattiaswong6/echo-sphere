27419169f356ea7a088a1da099edc73a19025e80
mattiaswong6 <151343938+mattiaswong6@users.noreply.github.com>
2025-01-18 15:10:18 -0800

    Basic page setup

---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-600 text-white shadow-md h-24 flex justify-around align-middle">
+      <a>
+          <img src="/next.svg"/>
+      </a>
+
+
+    </header>
+  )
+}
---
+++ frontend/src/app/listen/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Listen() {
+  return (
+    <main>
+        <h2>Listen</h2>
+    </main>
+  )
+}
