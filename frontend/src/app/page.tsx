e4caa9fa10f275f3e84c9afc669ce865a4a76c62
simonxu <simonxu2111@gmail.com>
2025-01-18 21:38:55 -0800

    Sidebar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,19 @@
+"use client"
+
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import {Link, Routes, BrowserRouter as Router, Route} from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
       <Header/>
-      <main>
-      
-      </main>
-      <footer>
-
-      </footer>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/'/>
+        </Routes>
+      </Router>
     </div>
   );
 }
