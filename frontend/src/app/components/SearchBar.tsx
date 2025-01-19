fa609a6d7c2fbe1c42b520a3b3f4e396e8767449
mattiaswong6 <151343938+mattiaswong6@users.noreply.github.com>
2025-01-18 16:16:51 -0800

    Basic header bar

--- frontend/src/app/components/SearchBar.tsx
+++ frontend/src/app/components/SearchBar.tsx
@@ -1,10 +1,24 @@
+'use client'
+
+import React, { useState } from "react";
+
 export default function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(e.target.value);
+      };
+    
+    //   const handleSearch = () => {
+    //     alert(`Searching for: ${searchQuery}`);
+    //   };
+
   return (
     <div>
         <input
             type="text"
-            // value={}
-            // onChange={}
+            value={searchQuery}
+            onChange={handleInputChange}
             placeholder="Search..."
             className="w-96 px-4 py-2 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
           />
